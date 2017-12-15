8fbe98d7f1bbb99806af4f8275874ab33006ac62
Kieran <kieran@arcticdesk.com>
2017-12-15 23:53:53 +0000

    Fix phpunit namespacing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,3 +30,18 @@ use Carbon\Carbon;
 date_default_timezone_set('UTC');
 
 Carbon::setTestNow(Carbon::now());
+
+/*
+|--------------------------------------------------------------------------
+| PHPUnit 4/5/6 Support
+|--------------------------------------------------------------------------
+|
+| PHPUnit 6 introduced a breaking change that removed 
+| PHPUnit_Framework_TestCase as a base class, and replaced it with
+| \PHPUnit\Framework\TestCase 
+|
+*/
+
+if (! class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
