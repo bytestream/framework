8acb0afbe0988b60309951720807700f2e758dd1
Kieran <kieran@arcticdesk.com>
2017-12-16 00:15:34 +0000

    Attempt 2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -42,6 +42,8 @@ Carbon::setTestNow(Carbon::now());
 |
 */
 
-if (! class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
+class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+class_alias('\PHPUnit\Framework\Assert', 'PHPUnit_Framework_Assert');
+class_alias('\PHPUnit\Framework\ExpectationFailedException', 'PHPUnit_Framework_ExpectationFailedException');
+class_alias('\PHPUnit\Framework\Constraint\LogicalNot', 'PHPUnit_Framework_Constraint_Not');
+class_alias('\PHPUnit\Framework\Constraint\Constraint', 'PHPUnit_Framework_Constraint');
