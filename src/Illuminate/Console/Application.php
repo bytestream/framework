2f24660a81c2b9eb9f203d631426029e5bb5d571
Kieran <kieran.brahney@gmail.com>
2021-08-24 12:51:54 +0100

    Include args with php binary

--- src/Illuminate/Console/Application.php
+++ src/Illuminate/Console/Application.php
@@ -105,7 +105,7 @@ class Application extends SymfonyApplication implements ApplicationContract
      */
     public static function phpBinary()
     {
-        return ProcessUtils::escapeArgument((new PhpExecutableFinder)->find(false));
+        return ProcessUtils::escapeArgument((new PhpExecutableFinder)->find());
     }
 
     /**
